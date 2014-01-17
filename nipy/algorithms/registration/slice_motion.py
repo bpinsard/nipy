fix important bugs of shiftmap scaling

nipy/algorithms/registration/slice_motion.py
@@ -128,12 +128,10 @@ def compute_sigloss(fieldmap, fmap_reg,
     sv = np.zeros(3)
     sv[slicing_axis] = 1
     world_shift = fmri2wld[:3,:3].dot(sv)
-    shift_points[...,0] = nb.affines.apply_affine(
-        wld2fmap,
-        pts-world_shift[np.newaxis,np.newaxis,np.newaxis,:])
-    shift_points[...,1] = nb.affines.apply_affine(
-        wld2fmap,
-        pts+world_shift[np.newaxis,np.newaxis,np.newaxis,:])
+    fmap_shift = wld2fmap[:3,:3].dot(world_shift)
+    shift_points[:] = nb.affines.apply_affine(wld2fmap, pts)[...,np.newaxis]
+    shift_points[...,0] -= fmap_shift[np.newaxis,np.newaxis,np.newaxis,:]
+    shift_points[...,1] += fmap_shift[np.newaxis,np.newaxis,np.newaxis,:]
     tmp = fieldmap.get_data().copy()
     tmp[mask==0] = np.nan
     pts = nb.affines.apply_affine(wld2fmap,pts)
@@ -156,6 +154,32 @@ def compute_sigloss(fieldmap, fmap_reg,
     del lrgradients, sinc, theta, re, im, pts, fmap_values, tmp
     sigloss[np.isnan(sigloss)]=0
     return sigloss
+
+
+def intensity_factor(fieldmap, fmap_reg,
+                     mask,
+                     reg,fmat,pts,
+                     shift_factor, pe_dir, order=0):
+    fmri2wld = reg.dot(fmat)
+    wld2fmap = np.linalg.inv(fmap_reg.dot(fieldmap.get_affine()))
+    shift_points = np.empty(pts.shape+(2,))
+    sv = np.zeros(3)
+    sv[pe_dir] = 1
+    world_shift = fmri2wld[:3,:3].dot(sv)
+    fmap_shift = wld2fmap[:3,:3].dot(world_shift)
+    shift_points[:] = nb.affines.apply_affine(wld2fmap, pts)[...,np.newaxis]
+    shift_points[...,0] -= fmap_shift[np.newaxis,np.newaxis,np.newaxis,:]
+    shift_points[...,1] += fmap_shift[np.newaxis,np.newaxis,np.newaxis,:]
+
+    tmp = fieldmap.get_data().copy()
+    tmp[mask==0] = np.nan
+    pts = nb.affines.apply_affine(wld2fmap,pts)
+    fmap_values = map_coordinates(
+        tmp, np.concatenate(
+            (np.rollaxis(pts,-1,0)[...,np.newaxis],
+             np.rollaxis(shift_points,-2,0)),-1).reshape(3,-1),
+        order=order,cval=np.nan).reshape(pts.shape[:-1]+(3,))
+    return (fmap_values[...,-1]-fmap_values[...,-2])*shift_factor/2
 
 class EPIInterpolation(object):
 